62a10d56908dc797e18ea856b2e6532717d6ad8e
lucas-gtr <l_gottar@orange.fr>
2025-07-06 16:15:08 +0200

    Fix variable case in tests

--- tests/ExampleTests.cpp
+++ tests/ExampleTests.cpp
@@ -3,17 +3,17 @@
 
 TEST(ExampleTest, AddPositiveNumbers) {
     Example example;
-    EXPECT_EQ(example.add(2, 3), 5);
+    EXPECT_EQ(example.Add(2, 3), 5);
 }
 
 TEST(ExampleTest, AddNegativeNumbers) {
     Example example;
-    EXPECT_EQ(example.add(-4, -6), -10);
+    EXPECT_EQ(example.Add(-4, -6), -10);
 }
 
 TEST(ExampleTest, AddZero) {
     Example example;
-    EXPECT_EQ(example.add(0, 0), 0);
-    EXPECT_EQ(example.add(5, 0), 5);
-    EXPECT_EQ(example.add(0, -3), -3);
+    EXPECT_EQ(example.Add(0, 0), 0);
+    EXPECT_EQ(example.Add(5, 0), 5);
+    EXPECT_EQ(example.Add(0, -3), -3);
 }
