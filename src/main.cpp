4561e1708f49a99b422a16cf2d3207a113178231
lucas-gtr <l_gottar@orange.fr>
2025-07-06 16:13:21 +0200

    Fix variable case

--- src/main.cpp
+++ src/main.cpp
@@ -34,7 +34,7 @@ int main(int argc, char* argv[]) {
   int a = 5;
   int b = 10;
 
-  int result = calculator.add(a, b);
+  int result = calculator.Add(a, b);
 
   std::cout << "Result of addition: " << result << std::endl;
 
